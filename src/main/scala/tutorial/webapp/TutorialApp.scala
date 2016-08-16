6a620f5fcd1050f862e19e6e4f1d2bedf6279c8b
Ganesh Chand <gchand@guidewire.com>
2016-08-15 22:46:00 -0700

    Add Tutorial App and print a hello world message on console

---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,12 @@
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+
+/**
+  * Created by ganeshchand on 8/15/16.
+  */
+object TutorialApp extends JSApp {
+  override def main(): Unit = {
+    println("Hello World")
+  }
+}
