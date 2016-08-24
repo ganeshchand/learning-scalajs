eed26c92a6e033ded5950ef62c7e816eaaa24c6c
Ganesh Chand <gchand@guidewire.com>
2016-08-23 21:00:50 -0700

    simple search box example

--- build.sbt
+++ build.sbt
@@ -1,4 +1,9 @@
+import com.lihaoyi.workbench.Plugin._
+
 enablePlugins(ScalaJSPlugin)
+
+workbenchSettings
+
 
 name := "scalajs-basic-tutorial"
 
@@ -6,13 +11,14 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-persistLauncher in Compile := true
-
-persistLauncher in Test := true
+//persistLauncher in Compile := true
+//
+//persistLauncher in Test := true
 
 //%%% means this is not a normal Scala library
 libraryDependencies ++= Seq("org.scala-js" %%% "scalajs-dom" % "0.9.0",
   "com.lihaoyi" %%% "scalatags" % "0.6.0")
 
+bootSnippet := "tutorial.webapp.ScalaJSExample().main(document.getElementById('container'))"
 
-    
+updateBrowsers <<= updateBrowsers.triggeredBy(fastOptJS in Compile)
--- src/main/scala/tutorial/webapp/ScalaJSExample.scala
+++ src/main/scala/tutorial/webapp/ScalaJSExample.scala
@@ -3,7 +3,10 @@ package tutorial.webapp
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
 import org.scalajs.dom.html
+import org.w3c.dom.Attr
+
 import scalatags.JsDom.all._
+
 /**
   * Created by ganeshchand on 8/20/16.
   */
@@ -11,11 +14,47 @@ import scalatags.JsDom.all._
 object ScalaJSExample {
   @JSExport
   def main(container: html.Div): Unit = {
+
     container.innerHTML = ""
+    val myInput = input.render
+    val output = div.render
+
+    val fruits = Seq(
+      "Apple",
+      "Banana",
+      "Pineapple",
+      "Mango",
+      "Mangosteen",
+      "Mandarin"
+    )
+    myInput.onkeyup = (e: dom.Event) => {
+      //      println(myInput.value)
+      //      output.textContent = myInput.value
+      output.innerHTML = ""
+      output.appendChild(
+        ul(
+          for {fruit <- fruits
+               if (fruit.toLowerCase().contains(myInput.value))
+
+          } yield {
+
+            val (matchedText, remainingText) = fruit.splitAt(myInput.value.length)
+            li(
+              span(matchedText, backgroundColor := "yellow"),
+              remainingText
+            )
+          }
+        ).render
+
+      )
+    }
+
     container.appendChild(
       div(
         h1("Hello World"),
-        p("Scalajs example using scalatags")
+        p("Scalajs example using scalatags"),
+        myInput,
+        output
       ).render
     )
   }
